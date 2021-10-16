001039d35b98a4eadd524bed7e19ced987f73b3b
Gabriel Castilho Mazzeu <gabriel.mazzeu@teknisa.com>
2021-10-16 20:38:32 -0300

    Refactoring and starting input formatting

--- src/index.js
+++ src/index.js
@@ -47,6 +47,7 @@ function removeDuplicateColumnsAndTransform(header, content) {
 
     content.forEach(line => {
         registry = line.split(',');
+        console.log(registry);
         personInfo = {...indexes};
         for(index in indexes) {
             personInfo[index] = '';
@@ -81,7 +82,8 @@ async function transformCSV(rawData) {
     try {
         const preFormatedData = parse(rawData, {
             columns: true,
-            skip_empty_lines: false
+            skip_empty_lines: false,
+            columnsDuplicatesToArray: true
         });
 
         return preFormatedData;
@@ -90,13 +92,79 @@ async function transformCSV(rawData) {
     }
 }
 
+function mergeRepeatedRegistries(registries) {
+    const unifiedRegistry = {};
+
+    unifiedRegistry['fullname'] = registries[0]['fullname'];
+    unifiedRegistry['eid'] = registries[0]['eid'];
+    registries.forEach(registry => {
+        for(key in registry) {
+            if(key.indexOf('email') != -1 || key.indexOf('phone') != -1) {
+                unifiedRegistry[key] = registry[key];
+            }
+        }
+    });
+    unifiedRegistry['group'] = [];
+    registries.forEach(registry => {
+        for(key in registry) {
+            if(key == 'group') {
+                unifiedRegistry[key] = lodash.concat(unifiedRegistry['group'],  registry[key]);
+            }
+        }
+    });
+    unifiedRegistry['group'] = lodash.join(unifiedRegistry['group'], '/');
+    unifiedRegistry['invisible'] = registries[0]['invisible'];
+    unifiedRegistry['see_all'] = registries[0]['see_all'];
+
+    return unifiedRegistry
+}
+
 /**
  * 
  * @param {Array} registries 
  */
 async function removeRegistryDuplicates(registries) {
 
-    return lodash.unionBy(registries, "eid");
+    const organizedRegistries = lodash.groupBy(registries, "eid");
+    
+    const uniqueRegistries = [];
+    for( registry in organizedRegistries ) {
+        if(organizedRegistries[registry].length == 1) {
+            if(Array.isArray(organizedRegistries[registry][0]['group'])) {
+                organizedRegistries[registry][0]['group'] = lodash.join(organizedRegistries[registry][0]['group'], '/');
+            }
+            uniqueRegistries.push(organizedRegistries[registry][0]);
+        }
+        else {
+            uniqueRegistries.push(mergeRepeatedRegistries(organizedRegistries[registry]));
+        }
+    }
+
+    return uniqueRegistries;
+}
+
+async function formatGroupAttribute(registries) {
+    
+    let formattedGroups;
+    registries.forEach((registry, index) => {
+        formattedGroups = registry['group'].split(/[/,]/); // Cut group through a regex
+        registries[index]['group'] = [] // Clear the registry
+        formattedGroups.forEach((group) => { // Trim the splited strings
+            if(group != '')
+                 registries[index]['group'].push(group.trim());
+        });
+
+        // Remove a repeated group
+        registries[index]['group'] = registries[index]['group'].filter((value, index, self) => {
+            return self.indexOf(value) == index;
+        })
+
+        // Sort the groups
+        registries[index]['group'].sort();
+    });
+    
+
+    return registries;
 }
 
 function writeOutput(data, fileName) {
@@ -118,13 +186,13 @@ function writeOutput(data, fileName) {
         
         const rawData = await readInput(inputFile);
 
-        const peopleInfo = await removeDuplicatesAndConvert(rawData);
+        const objects = await transformCSV(rawData);
 
-        // const objects = await transformCSV(rawData);
+        const uniqueRegistries = await removeRegistryDuplicates(objects);
 
-        // const uniqueRegistries = await removeRegistryDuplicates(objects);
-        
-        writeOutput(peopleInfo, 'output.json');
+        const preFormatedRegistries = await formatGroupAttribute(uniqueRegistries);
+
+        writeOutput(preFormatedRegistries, 'output.json');
         
     } catch (err) {
         console.error(`Something went wrong: ${err}`);
